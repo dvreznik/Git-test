b7f86b5fb1e8617c57cee2f5b873baf11065c78e
Dima <dvreznik@gmail.com>
2015-06-30 09:43:30 +0300

    Update Main.java
    ihave just changed a println

--- Git-repository/src/ua/dvreznik/test/Main.java
+++ Git-repository/src/ua/dvreznik/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello from git-hub !!!!");
+		System.out.println("Hello from Ivano-Frankovsk !!!!");
 	}
 
 }
